d09982b8f7e48bf4823d5cb240e3fd7068978cc1
Stevie1532 <stephen.asiamah@innorik.com>
2025-01-21 15:50:17 +0000

    added changes

--- src/components/common/feedBacks.tsx
+++ src/components/common/feedBacks.tsx
@@ -22,31 +22,35 @@ const FeedbackCard: React.FC<FeedbackCardProps> = ({
   className = "",
 }) => {
   return (
-    <div
-      className={`flex items-center justify-between bg-white p-6 mb-4 rounded-lg shadow-md ${className}`}
-    >
-      {/* Vote Button */}
-      <VoteButton
-        votes={votes}
-        onVote={(newVote) => onVote(id, newVote)}
-        className=""
-      />
+    <>
+      {
+        <div
+          className={`flex items-center justify-between bg-white p-6 mb-4 rounded-lg shadow-md ${className}`}
+        >
+          {/* Vote Button */}
+          <VoteButton
+            votes={votes}
+            onVote={(newVote) => onVote(id, newVote)}
+            className=""
+          />
 
-      {/* Feedback Content */}
-      <div className="ml-6 flex-1">
-        <h2 className="text-lg font-bold text-midnightBlue">{title}</h2>
-        <p className="text-darkGray">{description}</p>
-        <span className="inline-block mt-3 px-3 py-1 text-sm text-blue bg-gray rounded-lg">
-          {category}
-        </span>
-      </div>
+          {/* Feedback Content */}
+          <div className="ml-6 flex-1">
+            <h2 className="text-lg font-bold text-midnightBlue">{title}</h2>
+            <p className="text-darkGray">{description}</p>
+            <span className="inline-block mt-3 px-3 py-1 text-sm text-blue bg-gray rounded-lg">
+              {category}
+            </span>
+          </div>
 
-      {/* Comments Section */}
-      <div className="flex items-center text-gray-600">
-        <span className="text-xl text-grey">💬</span>
-        <span className="ml-2 text-darkGray font-bold">{comments}</span>
-      </div>
-    </div>
+          {/* Comments Section */}
+          <div className="flex items-center text-gray-600">
+            <span className="text-xl text-grey">💬</span>
+            <span className="ml-2 text-darkGray font-bold">{comments}</span>
+          </div>
+        </div>
+      }
+    </>
   );
 };
 
